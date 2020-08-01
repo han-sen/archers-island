e349a3c9cc41aa027a30fb4b341d39c85b609839
Michael Hansen <michaelandhansen@gmail.com>
2020-08-01 13:25:44 -0400

    set up canvas

---
+++ js/app.js
@@ -0,0 +1,12 @@
+// <- *************************************************** ->
+//    APP MAIN
+// <- *************************************************** ->
+
+
+// initialize our canvas context & size
+
+let canvas = document.getElementById("gameScreen");
+let ctx = canvas.getContext("2d");
+
+const GAME_WIDTH = 800;
+const GAME_HEIGHT = 600;
